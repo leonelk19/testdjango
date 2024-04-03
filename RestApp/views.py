5cb734d78fdbd6a38fd50ae36e6eafe261d4162a
leonelk19 <leonelk19@leonelk19.leonelk19>
2024-04-03 21:49:31 +0100

    second commit update and delete

--- RestApp/views.py
+++ RestApp/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 
 @api_view(['GET','POST'])
 # Create your views here.
-def marketls(request):
+def marketls(request,format=None):
     if request.method =='GET':
         markets=market.objects.all()
         serializer=marketserializer(markets,many=True)
@@ -18,3 +18,22 @@ def marketls(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
+@api_view(['GET','PUT'])
+# Create your views here.
+def market_details(request,id,format=None):
+    try:
+        market.object.get(pk=id)
+    except market.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method =='GET':
+        serializer=marketserializer(market)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        serializer=marketserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        market.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
